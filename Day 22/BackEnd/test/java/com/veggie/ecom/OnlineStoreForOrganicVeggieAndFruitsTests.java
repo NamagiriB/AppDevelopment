849310d66a5077a8d4de99fede73a635e6541e97
NamagiriB <119105158+NamagiriB@users.noreply.github.com>
2023-10-16 11:29:32 +0530

    Add files via upload

---
+++ Day 22/BackEnd/test/java/com/veggie/ecom/OnlineStoreForOrganicVeggieAndFruitsTests.java
@@ -0,0 +1,13 @@
+package com.veggie.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreForOrganicVeggieAndFruitsTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
