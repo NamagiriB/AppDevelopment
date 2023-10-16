65968918fb14dc01976fb8b122d1078357351e53
NamagiriB <119105158+NamagiriB@users.noreply.github.com>
2023-10-16 11:23:49 +0530

    Delete Day 21/Frontend/Backend directory

--- Day 21/Frontend/Backend/main/java/com/example/OnlineStoreForOrganicVeggieAndFruits/model/enumerate/Role.java
+++
@@ -1,9 +0,0 @@
-package com.example.OnlineStoreForOrganicVeggieAndFruits.model.enumerate;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public enum Role {
-    ADMIN,
-    CUSTOMER
-}
